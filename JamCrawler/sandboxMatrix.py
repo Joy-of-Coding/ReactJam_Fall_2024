7642ebfac89e5e9280ff16119c1472cc8e0bbae1
JonAdams99 <jon.adams@rocketmail.com>
2024-10-01 01:51:31 -0600

    Matrix update, combat lvl one reached

--- JamCrawler/sandboxMatrix.py
+++ JamCrawler/sandboxMatrix.py
@@ -10,9 +10,9 @@ base = [10, 10, 5, 100] # Attack, Defense, Speed, Hit Points
 playerAttack = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28] # Levels
 playerDefense = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28] # Levels
 playerHitPoints = [100, 125, 150, 175, 200, 250, 300, 350, 400, 500] # Levels
-monsterAttack = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28] # Levels
-monsterDefense = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28] # Levels
-monsterHitPoints = [100, 125, 150, 175, 200, 250, 300, 350, 400, 500] # Levels
+monsterAttack = [2, 12, 14, 16, 18] # Dungeon Levels
+monsterDefense = [1, 2, 4, 6, 8] # Dungeon Levels
+monsterHitPoints = [5, 100, 150, 175, 200] # Levels
 speed = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14] # Levels
 expGain = [100, 125, 150, 175, 200, 250, 300, 350, 400, 500] # Levels
 weapon = [1, 3, 7, 15, 25] # Dungeon Levels
@@ -32,39 +32,61 @@ def defense( defense, armor, base, speed):
     return roundNum
 
 def hitPointCounter(hitPoint, attack, defense):
-    roundNum = hitPoint + (attack - defense)
+    roundNum = hitPoint - (attack - defense)
     return roundNum
 
-def combatRound(pLvl, mLvl, mNumber, pWpn, pArm):
-    playerAtk = attack(playerAttack[pLvl], pWpn, base[0], speed[pLvl])
-    playerDef = defense(playerDefense[pLvl], pArm, base[1], speed[pLvl])
+def combatRound(pLvl, mLvl, mNumber, pWpn, pArm, numberRounds, playerCombatRound, monsterCombatRound):
+    winner = ""
+    # if numberRounds == 1:
+    #     playerCombatRound = playerHitPoints[pLvl]
+    #     monsterCombatRound = monsterHitPoints[mLvl]
+    print("Round: ", numberRounds)
+    playerAtk = attack(playerAttack[pLvl-1], pWpn, base[0], spd(pLvl))
+    playerDef = defense(playerDefense[pLvl-1], pArm, base[1], spd(pLvl))
 
-    monsterAtk = (attack(monsterAttack[pLvl], pWpn, base[0], speed[pLvl])) * mNumber
-    monsterDef = defense(monsterDefense[pLvl], pArm, base[1], speed[pLvl])
-
-    playerCombat = hitPointCounter(playerHitPoints, playerAtk, monsterDef)
-
-
-    return winner, hitPointCounter
+    monsterAtk = (attack(monsterAttack[mLvl-1], 0, base[0], 0)) * mNumber
+    monsterDef = defense(monsterDefense[mLvl-1], 0, base[1], 0)
+    print(playerAtk, playerDef, monsterAtk, monsterDef)
+    playerCombat = hitPointCounter(playerCombatRound, monsterAtk, playerDef)
+    monsterCombat = hitPointCounter(monsterCombatRound , playerAtk, monsterDef)
+    
+    print("Player: ", playerCombat )
+    print("Monster: ", monsterCombat)
+    if monsterCombat >= 0 and playerCombat >= 0:
+        numberRounds += 1
+        playerCombatRound = playerCombat
+        monsterCombatRound = monsterCombat
+        combatRound(pLvl, mLvl, mNumber, pWpn, pArm, numberRounds, playerCombatRound, monsterCombatRound)
+    elif monsterCombat <= 0:
+        winner = "Player"
+        print(winner)
+    else:
+        winner = "You are dead, please try again... Sucker!"
+    return winner
 
 def experience(monsterExp, monsterNum, quest, dungeoncompletion):
     exp = (monsterExp*monsterExp) + quest + dungeoncompletion
     return exp
 
-def speed (level):
+def spd(level):
     speedMultiplier = speed[level]
     return speedMultiplier
 
 def main():
     pLvl=1
     mLvl=1
-    mNumber=1
+    mNumber=16 # 17 kills the player
+    winner = ""
     pWpn=weapon[0]
     pArm=armor[0]
-
+    num = 1
+    playerCombatRound = playerHitPoints[pLvl-1]
+    monsterCombatRound = monsterHitPoints[mLvl-1] * mNumber
     # user input for next combat round
-    winner, hitPointCounter = combatRound(pLvl, mLvl, mNumber, pWpn, pArm)
-    totalExperience = experience(monsterExp, monsterNum, quest, dungeoncompletion) 
+    winner = combatRound(pLvl, mLvl, mNumber, pWpn, pArm, num, playerCombatRound, monsterCombatRound)
+    print("the winner is: ", winner)
+    
+    # totalExperience = experience(monsterExp, monsterNum, quest, dungeoncompletion) 
 
 # Don't run main on import
 if __name__ == "__main__":
