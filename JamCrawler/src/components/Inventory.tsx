f28affcf43cb05ab885bc155196adb532a08dc7e
tbatchelder <pegacorn@optonline.net>
2024-10-02 19:53:28 -0400

    Inventory CSS updates

--- JamCrawler/src/components/Inventory.tsx
+++ JamCrawler/src/components/Inventory.tsx
@@ -1,7 +1,7 @@
 // src/components/Inventory.tsx
-import React from 'react';
-import { Item } from '../types/types';
-import './Inventory.css'
+import React from "react";
+import { Item } from "../types/types";
+import "./Inventory.css";
 
 type InventoryProps = {
   inventory: Item[];
@@ -9,13 +9,15 @@ type InventoryProps = {
 };
 
 const Inventory: React.FC<InventoryProps> = ({ inventory, useItem }) => (
-  <div className='inventory-container'>
+  <div className="inventory-container">
     <h3>Inventory</h3>
     {inventory.length > 0 ? (
       inventory.map((item, index) => (
-        <div className='inventory-item-container' key={index}>
-          <span>{item.name}</span>
-          <button onClick={() => useItem(index)}>Use</button>
+        <div className="grid-container" key={index}>
+          <div className="grid-item inventory-item-name">{item.name}</div>
+          <div className="grid-item">
+            <button onClick={() => useItem(index)}>Use</button>
+          </div>
         </div>
       ))
     ) : (
