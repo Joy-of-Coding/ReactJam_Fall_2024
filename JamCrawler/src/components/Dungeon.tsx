c0afad31acc1f20f7638d3ec2f1882d79c9bdd49
PlasticMafia <157663931+PlasticMafia@users.noreply.github.com>
2024-10-02 01:03:42 -0400

    Updated player and monster icons

--- JamCrawler/src/components/Dungeon.tsx
+++ JamCrawler/src/components/Dungeon.tsx
@@ -20,8 +20,8 @@ const Dungeon: React.FC<DungeonProps> = ({ dungeon, player, monster }) => {
           key={`${x}-${y}`}
           style={{ width: '20px', display: 'inline-block', textAlign: 'center' }}
         >
-          {x === player.position.x && y === player.position.y ? '@' : 
-           x === monster.position.x && y === monster.position.y ? 'M' : cell}
+          {x === player.position.x && y === player.position.y ? '🧑‍🌾' : 
+           x === monster.position.x && y === monster.position.y ? '🕷️' : cell}
         </span>
       ))}
     </div>
