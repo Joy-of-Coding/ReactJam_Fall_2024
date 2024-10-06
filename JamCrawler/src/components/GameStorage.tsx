1f81539790d7e4989c38a371ad315152bf1d1090
tbatchelder <pegacorn@optonline.net>
2024-10-06 14:39:03 -0400

    Working prototype for localstorage

--- JamCrawler/src/components/GameStorage.tsx
+++ JamCrawler/src/components/GameStorage.tsx
@@ -1,85 +1,86 @@
-import React, { useEffect, useState } from "react";
-import { Player } from "../types/types";
-import { Monster } from "../types/types";
+// import React, { useEffect, useState } from "react";
+// import { Player } from "../types/types";
+// import { Monster } from "../types/types";
 // import { Item } from "../types/types";
-// import TodoList from "./TodoList";
+// // import TodoList from "./TodoList";
+// import App from "../App";
 
-type PlayerStatsProps = {
-  player: Player;
-};
+// // type PlayerStatsProps = {
+// //   player: Player;
+// // };
 
-type MonsterStatsProps = {
-  monster: Monster;
-};
+// // type MonsterStatsProps = {
+// //   monster: Monster;
+// // };
 
-// type InventoryProps = {
-//   inventory: Item[];
-//   useItem: (index: number) => void;
+// // type InventoryProps = {
+// //   inventory: Item[];
+// //   useItem: (index: number) => void;
+// // };
+
+// // interface Todo {
+// //   text: string;
+// //   priority: 'high' | 'medium' | 'low';
+// //   complete: boolean;
+// // }
+
+// const GameStorage: React.FC<PlayerStatsProps> = ({ player }) => {
+//   const [gameDict, setGameDict] = useState({});
+
+//   useEffect(() => {
+//     const storedDict = localStorage.getItem("gameDict");
+
+//     if (storedDict) {
+//       const gameStateName = [
+//         "playerPosition",
+//         "playerStrength",
+//         "playerDefense",
+//         "playerCurrentHealth",
+//         "playerMaxHealth",
+//         "playerXP",
+//         "playerInventory",
+//         "playerAlive",
+//         "monsterPosition",
+//         "monsterAlive",
+//         "monsterStrength",
+//         "monsterDefense",
+//         "monsterHealth",
+//         "monsterLuck",
+//         "monsterInventory",
+//         "gameLevel",
+//       ];
+//       const gameStateValue = [
+//         player.position,
+//         player.strength,
+//         player.defense,
+//         player.health,
+//         player.maxHealth,
+//         player.experience,
+//         player.inventory,
+//         player.isAlive,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//       ];
+//       for (let i = 0; i < gameStateName.length; i++) {
+//         gameDict[gameStateName[i]] = gameStateValue[i];
+//       }
+
+//       setGameDict(JSON.parse(storedDict));
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem("gameDict", JSON.stringify(gameDict));
+//     console.log(localStorage.getItem("gameDict"));
+//   }, [gameDict]);
+
+//   return <div></div>;
 // };
 
-// interface Todo {
-//   text: string;
-//   priority: 'high' | 'medium' | 'low';
-//   complete: boolean;
-// }
-
-const GameStorage: React.FC<PlayerStatsProps> = ({ player }) => {
-  const [gameDict, setGameDict] = useState({});
-
-  useEffect(() => {
-    const storedDict = localStorage.getItem("gameDict");
-
-    if (storedDict) {
-      const gameStateName = [
-        "playerPosition",
-        "playerStrength",
-        "playerDefense",
-        "playerCurrentHealth",
-        "playerMaxHealth",
-        "playerXP",
-        "playerInventory",
-        "playerAlive",
-        "monsterPosition",
-        "monsterAlive",
-        "monsterStrength",
-        "monsterDefense",
-        "monsterHealth",
-        "monsterLuck",
-        "monsterInventory",
-        "gameLevel",
-      ];
-      const gameStateValue = [
-        player.position,
-        player.strength,
-        player.defense,
-        player.health,
-        player.maxHealth,
-        player.experience,
-        player.inventory,
-        player.isAlive,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-      ];
-      for (let i = 0; i < gameStateName.length; i++) {
-        gameDict[gameStateName[i]] = gameStateValue[i];
-      }
-
-      setGameDict(JSON.parse(storedDict));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("gameDict", JSON.stringify(gameDict));
-    console.log(localStorage.getItem("gameDict"));
-  }, [gameDict]);
-
-  return <div></div>;
-};
-
-export default GameStorage;
+// export default GameStorage;
