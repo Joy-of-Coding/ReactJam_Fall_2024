d2b674d98d887b36d0d671bb5017f4bc932453eb
tbatchelder <pegacorn@optonline.net>
2024-10-06 00:00:59 -0400

    Slowly getting this working but it's a struggle

--- JamCrawler/src/components/GameStorage.tsx
+++ JamCrawler/src/components/GameStorage.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Player } from "../types/types";
-import { Monster } from '../types/types';
-import { Item } from "../types/types";
-import { TodoList } from "./components/TodoList";
+import { Monster } from "../types/types";
+// import { Item } from "../types/types";
+// import TodoList from "./TodoList";
 
 type PlayerStatsProps = {
   player: Player;
@@ -12,74 +12,74 @@ type MonsterStatsProps = {
   monster: Monster;
 };
 
-type InventoryProps = {
-  inventory: Item[];
-  useItem: (index: number) => void;
+// type InventoryProps = {
+//   inventory: Item[];
+//   useItem: (index: number) => void;
+// };
+
+// interface Todo {
+//   text: string;
+//   priority: 'high' | 'medium' | 'low';
+//   complete: boolean;
+// }
+
+const GameStorage: React.FC<PlayerStatsProps> = ({ player }) => {
+  const [gameDict, setGameDict] = useState({});
+
+  useEffect(() => {
+    const storedDict = localStorage.getItem("gameDict");
+
+    if (storedDict) {
+      const gameStateName = [
+        "playerPosition",
+        "playerStrength",
+        "playerDefense",
+        "playerCurrentHealth",
+        "playerMaxHealth",
+        "playerXP",
+        "playerInventory",
+        "playerAlive",
+        "monsterPosition",
+        "monsterAlive",
+        "monsterStrength",
+        "monsterDefense",
+        "monsterHealth",
+        "monsterLuck",
+        "monsterInventory",
+        "gameLevel",
+      ];
+      const gameStateValue = [
+        player.position,
+        player.strength,
+        player.defense,
+        player.health,
+        player.maxHealth,
+        player.experience,
+        player.inventory,
+        player.isAlive,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ];
+      for (let i = 0; i < gameStateName.length; i++) {
+        gameDict[gameStateName[i]] = gameStateValue[i];
+      }
+
+      setGameDict(JSON.parse(storedDict));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("gameDict", JSON.stringify(gameDict));
+    console.log(localStorage.getItem("gameDict"));
+  }, [gameDict]);
+
+  return <div></div>;
 };
 
-interface Todo {
-  text: string;
-  priority: 'high' | 'medium' | 'low';
-  complete: boolean;
-}
-
-const gameState = {
-  playerPosition: 0,
-  playerStrength: 0,
-  playerDefense: 0,
-  playerCurrentHealth: 0,
-  playerMaxHealth: 0,
-  playerXP: 0,
-  playerInventory: 0,
-  playerAlive: true,
-  monsterPosition: 0,
-  monsterAlive: true,
-  monsterStrength: 0,
-  monsterDefense: 0,
-  monsterHealth: 0,
-  monsterLuck: 0,
-  monsterInventory: 0,
-  gameLevel: 0,
-
-  };
-
-const PlayerStats: React.FC<PlayerStatsProps> = ({ player }) => (
-  gameState.playerStrength = { player.strength };
-  gameState.playerDefense = { player.defense };
-  gameState.playerCurrentHealth =: { player.health };
-  gameState.playerMaxHealth = { player.maxHealth };
-  gameState.playerXP = { player.experience };
-
-
-  const [playerStats, setPlayerStats] = useState(false);
-
-  const storeGame = () => {
-    setPlayerStats(true);
-    localStorage.setItem("Player", JSON.stringify(gameState));
-    console.log(JSON.parse(localStorage.getItem("Player") || ""));
-  };
-  const restoreGame = () => {
-    const playerRestore = JSON.parse(localStorage.getItem("gameState") || "");
-    console.log(playerRestore);
-  };
-  const removeGame = () => {
-    localStorage.clear();
-  };
-
-  return (
-    <>
-      <div className="App">
-        <div>
-          <button onClick={storeGame}>Done</button>
-        </div>
-        <div>
-          <button onClick={restoreGame}>Restore</button>
-        </div>
-        <div>
-          <button onClick={removeGame}>Remove</button>
-        </div>
-      </div>
-    </>
-  );
-}
 export default GameStorage;
